0898dbcc8f2f27512bb1cdde5a17f80bb47777a1
Ramzi Youssef <contact@mediacept.com>
2018-09-05 13:29:55 +0100

    Updates

--- src/common/assets/database/schema/0.0.3/initData.js
+++ src/common/assets/database/schema/0.0.3/initData.js
@@ -20,70 +20,77 @@ const fse = require('fs-extra');
 
 
 
-let schemaModels = require("./models")
+let schemaModels = require("./models/index.js");
+console.log("schemaModels", schemaModels)
 
 const dbFilename = path.join(userDataPath, 'database/mc-office.sqlite');
 var knex = require('knex')({ client: 'sqlite3', connection: { filename: dbFilename }, useNullAsDefault: true });
 
 
 
-  // ==============initTable===========
-  function initTable(tableName) {
-    return new Promise(function(resolve, reject) {
-        knex.schema.hasTable(tableName).then(function(exists) {
-          if (!exists) {
-            var tableSchemaModel = require("./models/" + schemaModels[tableName])
-            knex.schema.createTable(tableName, tableSchemaModel).then(() => {
-              console.log("Table '" + tableName + "' created.");
-              countTableItems(tableName)
-            })
-          } else {
-            countTableItems(tableName)
-          }
+// ==============initTable===========
+function initTable(tableName) {
+  return new Promise(function(resolve, reject) {
+    knex.schema.hasTable(tableName).then(function(exists) {
+      if (!exists) {
+        var tableSchemaModel = require(path.resolve(__dirname, "./models/" + tableName + ".js"));
+        knex.schema.createTable(tableName, tableSchemaModel).then(() => {
+          console.log("Table '" + tableName + "' created.");
+          countTableItems(tableName)
         })
+      } else {
+        countTableItems(tableName)
+      }
+    })
 
-        function countTableItems(tableName) {
-          knex(tableName).count('* as count').then(c => {
-            var total = c[0].count;
-            console.log("Total items in Table '" + tableName + "' =", total)
-            if (total == 0) { importDataFromSqlFile(tableName) } else { resolve(false) }
-          })
-        }
+    function countTableItems(tableName) {
+      knex(tableName).count('* as count').then(c => {
+        var total = c[0].count;
+        console.log("Total items in Table '" + tableName + "' =", total)
+        if (total == 0) { importDataFromSqlFile(tableName) } else { resolve(false) }
+      })
+    }
 
-        // ******************
-        function importDataFromSqlFile(tableName) {
-          var dataSqlFile = "./data/" + tableName + ".sql"
-          fs.exists(dataSqlFile, function(exists) {
-              if (exists) {
-                mainWindow.webContents.send("initApplicationData", 'start');
-                fs.readFile(dataSqlFile, function readFileCallback(err, sql) {
-                  knex.raw(sql).then(result => { resolve(true) })
-                });
-              } else { resolve(false) }
-            }
+    // ******************
+    function importDataFromSqlFile(tableName) {
+      var dataSqlFile = path.resolve(__dirname, "./data/" + tableName + ".sql");
+      console.log(tableName,"<==== dataSqlFile-------------->",dataSqlFile)
+      fs.exists(dataSqlFile, function(exists) {
+        if (exists) {
+          mainWindow.webContents.send("initApplicationData", 'start');
+          fs.readFile(dataSqlFile, "utf8", function readFileCallback(err, sql) {
+            console.log(tableName,"<==== sql-------------->",sql)
+            knex.raw(sql).then(result => { resolve(true) })
           });
-      }
-    }
-
-
-
-
-    function initDatabase() {
-      var databaseModels = schemaModels;
-      var funcs = []
-      for (var i = 0; i < databaseModels.lenght; i++) {
-        funcs.push(databaseModels[i] + '()')
-      }
-
-      var promises = Promise.all(funcs);
-      promises.then(function(values) {
-        console.log(databaseModels, values)
-        if (values.every((val, i, arr) => val === false)) {
-          mainWindow.webContents.send("initApplicationData", 'nothingtodo');
         } else {
-          mainWindow.webContents.send("initApplicationData", 'success');
+          resolve(false)
         }
+      })
+    };
+  })
+}
 
-      });
+
+
+
+function initDatabase() {
+  var databaseModels = schemaModels;
+  var funcs = []
+  for (var i = 0; i < databaseModels.length; i++) {
+    funcs.push(eval('initTable("' + databaseModels[i] + '")'))
+  }
+  console.log("funcs*************", funcs)
+  var promises = Promise.all(funcs);
+  promises.then(function(values) {
+    console.log(databaseModels, values)
+    if (values.every((val, i, arr) => val === false)) {
+      mainWindow.webContents.send("initApplicationData", 'nothingtodo');
+    } else {
+      mainWindow.webContents.send("initApplicationData", 'success');
     }
-    initDatabase() module.exports = {}
+
+  });
+};
+
+initDatabase();
+module.exports = {}
--- src/common/assets/database/schema/0.0.3/models/index.js
+++ src/common/assets/database/schema/0.0.3/models/index.js
@@ -3,12 +3,17 @@
  * in a one-shot manner. There should not be any reason to edit this file.
  */
 
-const files = require.context('.', false, /\.js$/)
-const schemaModels = {}
+const fs = require('fs');
+const path = require("path");
+var files = fs.readdirSync(path.resolve(__dirname, "./"))
+const schemaModels = []
 
-files.keys().forEach(key => {
-  if (key === './index.js') return
-  schemaModels[key.replace(/(\.\/|\.js)/g, '')] = files(key).default
+files.forEach(key => {
+  if (key === 'index.js') return
+  // schemaModels[key.replace(/(\.\/|\.js)/g, '')] = key
+  schemaModels.push(key.replace(/(\.\/|\.js)/g, ''))
 })
 
-export default schemaModels
+console.log("files", files)
+console.log("schemaModels", schemaModels)
+module.exports =schemaModels
